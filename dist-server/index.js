9c734ede375e153231813844bab02b88798d83a4
Alana <alanayee07@gmail.com>
2021-07-28 16:42:51 -0700

    Install socket.io dependency

--- dist-server/index.js
+++ dist-server/index.js
@@ -1,8 +1,31 @@
 "use strict";
 
-var app = require('./app');
+// const app = require('./app');
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`App is listening on port ${PORT}`);
+// });
+var express = require('express');
 
-var PORT = process.env.PORT || 3000;
-app.listen(PORT, function () {
-  console.log("App is listening on port ".concat(PORT));
+var app = express();
+
+var http = require('http').createServer(app);
+
+var io = require('socket.io')(http, {
+  cors: {
+    origin: ['http://localhost:3001']
+  }
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World from the server');
+});
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+});
+http.listen(3000, function () {
+  console.log('listening on *:3000');
 });
--- server/index.js
+++ server/index.js
@@ -1,7 +1,31 @@
-const app = require('./app');
+// const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`App is listening on port ${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`App is listening on port ${PORT}`);
+// });
+
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: ['http://localhost:3001']
+  }
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World from the server');
 });
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  })
+})
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+})
