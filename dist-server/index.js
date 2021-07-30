a9772763c47589a8721c7bdeac9018ca4dacf7c0
Alana <alanayee07@gmail.com>
2021-07-30 12:31:07 -0700

    set up basic socket.io

--- dist-server/index.js
+++ dist-server/index.js
@@ -4,26 +4,24 @@ var express = require('express');
 
 var http = require('http');
 
-var socketIo = require('socket.io');
-
-var port = process.env.PORT || 4001;
 var app = express();
-var server = http.createServer(app); // sets up a new server instance of socket.io
+var server = http.createServer(app);
+
+var socketIo = require('socket.io'); // sets up a new server instance of socket.io
+
 
 var io = socketIo(server);
+var port = process.env.PORT || 4001;
 app.get('/', function (req, res) {
   res.send('Hello World from the server');
 }); // set up connection event listener between server and the client
 
 io.on('connection', function (socket) {
   console.log('a user connected');
-  socket.on('incoming data', function (data) {
-    socket.broadcast.emit('outgoing data', {
-      num: data
-    });
-  });
-  socket.on('disconnect', function () {
-    console.log('user disconnected');
+  socket.emit("your id", socket.id);
+  socket.on('send message', function (body) {
+    // all clients connected should receive msg
+    io.emit("message", body);
   });
 });
 server.listen(port, function () {
--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
-
-const port = process.env.PORT || 4001;
-
 const app = express();
 const server = http.createServer(app);
+const socketIo = require('socket.io');
 // sets up a new server instance of socket.io
 const io = socketIo(server);
+
+const port = process.env.PORT || 4001;
 
 
 
@@ -18,12 +17,10 @@ app.get('/', (req, res) => {
 // set up connection event listener between server and the client
 io.on('connection', socket => {
   console.log('a user connected');
-  socket.on('incoming data', data => {
-    socket.broadcast.emit('outgoing data', {num: data})
-  });
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.emit("your id", socket.id);
+  socket.on('send message', body => {
+    // all clients connected should receive msg
+    io.emit("message", body)
   })
 })
 
