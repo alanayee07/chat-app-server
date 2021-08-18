23b776245da10856a24c558da790f3ca55a5c478
Alana <alanayee07@gmail.com>
2021-08-17 19:47:14 -0700

    created obj2 and messages appear on fe

--- dist-server/index.js
+++ dist-server/index.js
@@ -7,25 +7,63 @@ var http = require('http');
 var app = express();
 var server = http.createServer(app);
 
-var socketIo = require('socket.io'); // sets up a new server instance of socket.io
+var socket = require('socket.io'); // sets up a new server instance of socket.io
 
 
-var io = socketIo(server, {
+var io = socket(server, {
   cors: {
     origin: '*'
   }
 });
-app.get('/', function (req, res) {
-  res.send('Hello World from the server');
-}); // set up connection event listener between server and the client
+var rooms = ['Alana-Room', 'Phong-Room', 'Matt-Room'];
+var PORT = process.env.PORT || 7000; // set up connection event listener between server and the client
 
 io.on('connection', function (socket) {
-  console.log('a user connected');
-  socket.on('message', function (body) {
-    console.log('Message received on server: ', body);
-    io.emit('message', body);
+  // const { id } = socket.client;
+  // console.log(`User connected: ${id}`);
+  console.log('socket obj: ', socket);
+  console.log('user connected on socketID: ', socket.id);
+  var obj2 = {
+    rooms: rooms
+  }; // we send back obj2 to room
+
+  console.log('this is obj2: ', obj2);
+  io.to(socket.id).emit('message', obj2); // broadcast message to 1 user connected
+
+  socket.on("message", function (userObj, msg) {
+    console.log('socketID: ', socket.id);
+    console.log('userObj', userObj);
+
+    if (userObj.room) {
+      socket.join(userObj.room);
+      console.log('user has joined this room: ', socket.room);
+      var obj = {
+        message: userObj.message,
+        comment: 'this is coming from the server'
+      };
+      io.to(userObj.room).emit('message', obj);
+    } // and then later
+    // io.to(userId).emit('hi');
+    // console.log(`Message received on server from ID: ${id}: ${msg}`);
+    // socket.to(anotherSocketId).emit("message", socket.id, msg);
+    // io.emit("message", msg);
+
+  }); // socket.on('join_room', roomId => {
+  //   socket.join(roomId);
+  //   console.log('User joined room: ' + roomId);
+  // })
+  // broadcast to all clients when a user connects
+  // socket.broadcast.emit("message", 'A user has joined the chat');
+  // when user disconnects
+
+  socket.on("disconnect", function () {
+    console.log('USER DISCONNECTED');
+    io.emit("message", 'a user has left the chat');
   });
 });
-server.listen(7000, function () {
-  console.log("Listening on port 7000");
+app.get('/', function (req, res) {
+  res.send('Hello World from the server');
+});
+server.listen(PORT, function () {
+  console.log("Listening on port ".concat(PORT));
 });
--- server/index.js
+++ server/index.js
@@ -2,29 +2,71 @@ const express = require('express');
 const http = require('http');
 const app = express();
 const server = http.createServer(app);
-const socketIo = require('socket.io');
+const socket = require('socket.io');
 // sets up a new server instance of socket.io
-const io = socketIo(server, {
+const io = socket(server, {
   cors: {
     origin:'*',
   }
 })
 
+const rooms = ['Alana-Room', 'Phong-Room', 'Matt-Room'];
 
+const PORT = process.env.PORT || 7000;
+
+// set up connection event listener between server and the client
+io.on('connection', (socket) => {
+  // const { id } = socket.client;
+  // console.log(`User connected: ${id}`);
+  console.log('socket obj: ', socket);
+  console.log('user connected on socketID: ', socket.id);
+  const obj2 = {
+    rooms
+  }
+  // we send back obj2 to room
+  console.log('this is obj2: ', obj2);
+  io.to(socket.id).emit('message', obj2);
+  // broadcast message to 1 user connected
+  socket.on("message", (userObj, msg) => {
+    console.log('socketID: ', socket.id);
+    console.log('userObj', userObj);
+
+    if (userObj.room) {
+      socket.join(userObj.room);
+      console.log('user has joined this room: ', socket.room);
+      const obj = {
+        message: userObj.message,
+        comment: 'this is coming from the server',
+      }
+      io.to(userObj.room).emit('message', obj);
+    }
+
+    // and then later
+    // io.to(userId).emit('hi');
+    // console.log(`Message received on server from ID: ${id}: ${msg}`);
+    // socket.to(anotherSocketId).emit("message", socket.id, msg);
+    // io.emit("message", msg);
+  })
+
+  // socket.on('join_room', roomId => {
+  //   socket.join(roomId);
+  //   console.log('User joined room: ' + roomId);
+  // })
+
+  // broadcast to all clients when a user connects
+  // socket.broadcast.emit("message", 'A user has joined the chat');
+
+  // when user disconnects
+  socket.on("disconnect", () => {
+    console.log('USER DISCONNECTED');
+    io.emit("message", 'a user has left the chat');
+  })
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World from the server');
 });
 
-// set up connection event listener between server and the client
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('message', body => {
-    console.log('Message received on server: ', body);
-    io.emit('message', body)
-  })
-})
-
-server.listen(7000, () => {
-  console.log(`Listening on port 7000`);
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 })
